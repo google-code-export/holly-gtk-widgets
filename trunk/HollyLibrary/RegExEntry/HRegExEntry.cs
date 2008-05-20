e284d742163841d09ba79717b5771cf802325e39
fr.daniil <fr.daniil@9c20132d-584d-0410-8be9-414f904cf137>
2008-05-20 10:21:19 +0000

    hregexentry bug fixed

---
+++ trunk/HollyLibrary/RegExEntry/HRegExEntry.cs
@@ -0,0 +1,19 @@
+// HRegExEntry.cs created with MonoDevelop
+// User: dantes at 1:20 PM 5/20/2008
+//
+
+using System;
+
+namespace HollyLibrary
+{
+	
+	
+	public partial class HRegExEntry : Gtk.Bin
+	{
+		
+		public HRegExEntry()
+		{
+			this.Build();
+		}
+	}
+}
