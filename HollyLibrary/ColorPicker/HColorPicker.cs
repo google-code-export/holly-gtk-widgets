26a26b0bceeadee7e2a7006557c9c6592142278d
fr.daniil <fr.daniil@9c20132d-584d-0410-8be9-414f904cf137>
2008-05-20 10:31:19 +0000

    added the HColorPicker widget

---
+++ HollyLibrary/ColorPicker/HColorPicker.cs
@@ -0,0 +1,19 @@
+// HColorPicker.cs created with MonoDevelop
+// User: dantes at 1:30 PM 5/20/2008
+//
+
+using System;
+
+namespace HollyLibrary
+{
+	
+	
+	public partial class HColorPicker : Gtk.Bin
+	{
+		
+		public HColorPicker()
+		{
+			this.Build();
+		}
+	}
+}
