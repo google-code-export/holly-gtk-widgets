3c765cab2d1b0ff4b9df5c1a09aad23436636130
fr.daniil <fr.daniil@9c20132d-584d-0410-8be9-414f904cf137>
2008-05-23 18:43:40 +0000

    HToolTip more features and tests added

--- test/MainWin.cs
+++ test/MainWin.cs
@@ -19,11 +19,13 @@ namespace test
 			this.Build();
 			
 			this.hregexentry1.RegularExpression = "\\d{3}-\\d{2}-\\d{4}";
-			String text = "testoi asdlf jasd kfjasd fjad;s jfad;lsjfa kdsfal sfa jfad ;sfad sa; jfal jf\r\n asdjf l;akdjsf kdf ljadsf; kjasd; jfaf";
-			
-			HollyLibrary.HToolTip.AddToolTip( button2, "titlu 1", text );
-			HollyLibrary.HToolTip.AddToolTip( button3, "titlu 2", text + text );
-			HollyLibrary.HToolTip.AddToolTip( button4, "titlu 3", "buga buga" );
+			String text = "My first line of text \r\n";
+			text       += "Second line of text bla bla \r\n";
+			text       += "last line of text";
+			HollyLibrary.HToolTip.ToolTipInterval = 100;
+			HollyLibrary.HToolTip.AddToolTip( button2, "title 2", text, "gtk-yes" );
+			HollyLibrary.HToolTip.AddToolTip( button3, "title 3", text + text, System.Drawing.Color.White, System.Drawing.Color.Black );
+			HollyLibrary.HToolTip.AddToolTip( button4, "title 4", "buga buga", System.Drawing.Color.Yellow, System.Drawing.Color.Orange, "gtk-no" );
 		}
 
 		
