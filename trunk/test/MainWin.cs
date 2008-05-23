ca1835610bff94ba3aaba267266639ef9367523f
fr.daniil <fr.daniil@9c20132d-584d-0410-8be9-414f904cf137>
2008-05-23 12:01:09 +0000

    test with HToolTip

--- trunk/test/MainWin.cs
+++ trunk/test/MainWin.cs
@@ -3,6 +3,7 @@
 //
 
 using System;
+using System.Drawing;
 
 namespace test
 {
@@ -14,92 +15,15 @@ namespace test
 		public MainWin() : 
 				base(Gtk.WindowType.Toplevel)
 		{
+			this.Resize( 640, 480 );
 			this.Build();
+			
 			this.hregexentry1.RegularExpression = "\\d{3}-\\d{2}-\\d{4}";
-			this.AppPaintable = true;
-			this.Decorated    = false;
-			this.ExposeEvent += Expose;
-		}
-		
-		private  void Expose (object o, Gtk.ExposeEventArgs args)
-		{
-			int width;
-		int height;
-		
-		this.GetSize (out width, out height);
-		
-		// Create the cairo context
-		Cairo.Context cr = Gdk.CairoHelper.Create (this.GdkWindow);
-		
-		// Make the window transparent.
-		//if (supports_alpha)
-			cr.Color = new Cairo.Color (1.0, 1.0, 1.0, 0.0);
-		//else
-			//cr.Color = new Cairo.Color (1.0, 1.0, 1.0);
-		
-		cr.Operator = Cairo.Operator.Source;
-		cr.Paint ();
-		
-		//And draw everything we want
-		//Some cos/sin magic is done, never mind
-
- 		cr.Color = new Cairo.Color (1.0, 0.2, 0.2, 0.7);
- 		
- 		double radius = (double) (width < height ? width : height) / 2.0 - 0.8;
- 		double x_center = ((double) width) / 2.0;
- 		double y_center = ((double) height) / 2.0;
-		cr.Arc (x_center, y_center, radius, 0, 2.0*3.14);
-		cr.Fill ();
-		cr.Stroke ();
-		
-		cr.MoveTo (x_center, y_center);
-		//if (supports_alpha)
-			cr.Color = new Cairo.Color  (0.0, 0.0, 0.0, 0.9);
-		//else
-		//	cr.Color = new Cairo.Color  (0.0, 0.0, 0.0);
-		
-		System.DateTime time = System.DateTime.Now;
-		double hour = (double) time.Hour;
-		double minutes = (double) time.Minute;
-		double seconds = (double) time.Second;
-		double per_hour = (2.0 * 3.14) / 12.0;
-		double dh = (hour * per_hour) + (per_hour / 60.0) * minutes;
- 		dh += 2.0 * 3.14 / 4.0;
- 		cr.LineWidth = 0.05 * radius;
- 		cr.RelLineTo (-0.5 * radius * System.Math.Cos (dh), - 0.5 * radius * System.Math.Sin (dh));
- 		cr.MoveTo (x_center, y_center);
- 		
-		double per_minute = (2.0 * 3.14) / 60.0;
-		double dm = minutes * per_minute;
-		dm += 2.0 * 3.14 / 4.0;
-		cr.RelLineTo(-0.9 * radius * System.Math.Cos (dm), -0.9 * radius * System.Math.Sin (dm));
- 		cr.MoveTo (x_center, y_center);
- 		
-		double per_second = (2.0 * 3.14) / 60;
-		double ds = seconds * per_second;
-		ds += 2.0 * 3.14 / 4.0;
-		cr.RelLineTo(-0.9 * radius * System.Math.Cos (ds), -0.9 * radius * System.Math.Sin (ds));
-		cr.Stroke ();
-
-		//if (supports_alpha)
-			cr.Color = new Cairo.Color (0.5, 0.5, 0.5, 0.9);
-		//else
-		//	cr.Color = new Cairo.Color (0.5, 0.5, 0.5);
-		cr.Arc (x_center, y_center, 0.1 * radius, 0, 2.0*3.14);
-		cr.Fill ();
-		cr.Stroke ();
-
-		//Once everything has been drawn, create our XShape mask
-		//Our window content is contained inside the big circle,
-		//so let's use that one as our mask
-		Gdk.Pixmap pm = new Gdk.Pixmap (null, width, height, 1);
-		Cairo.Context pmcr =  Gdk.CairoHelper.Create (pm);
-		pmcr.Arc (x_center, y_center, radius, 0, 2.0*3.14);
-		pmcr.Fill ();
-		pmcr.Stroke ();
-		//Apply input mask
-		this.InputShapeCombineMask (pm, 0, 0);
-
+			String text = "testoi asdlf jasd kfjasd fjad;s jfad;lsjfa kdsfal sfa jfad ;sfad sa; jfal jf\r\n asdjf l;akdjsf kdf ljadsf; kjasd; jfaf";
+			
+			HollyLibrary.HToolTip.AddToolTip( button2, "titlu 1", text );
+			HollyLibrary.HToolTip.AddToolTip( button3, "titlu 2", text + text );
+			HollyLibrary.HToolTip.AddToolTip( button4, "titlu 3", "buga buga" );
 		}
 
 		
