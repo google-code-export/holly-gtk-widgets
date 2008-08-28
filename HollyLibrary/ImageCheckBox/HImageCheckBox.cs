d6839d8dae61b765d86e291c4aacae9331c656f3
fr.daniil <fr.daniil@9c20132d-584d-0410-8be9-414f904cf137>
2008-08-28 12:46:08 +0000

    *HImageCheckBox is now completed

--- HollyLibrary/ImageCheckBox/HImageCheckBox.cs
+++ HollyLibrary/ImageCheckBox/HImageCheckBox.cs
@@ -5,17 +5,120 @@
 //
 
 using System;
+using Gtk;
+using Gdk;
 
 namespace HollyLibrary
 {
 	
-	
 	public partial class HImageCheckBox : Gtk.Bin
 	{
+		Gtk.ToggleButton checkbutton = new Gtk.ToggleButton();
+		
+		public event EventHandler CheckedStateChanged;
+		Pixbuf checkedImage;
+		Pixbuf uncheckedImage;
+		bool _checked = false;
 		
 		public HImageCheckBox()
 		{
 			this.Build();
+			this.AppPaintable = true;
+			hbox1.PackStart( checkbutton );
+			this.ECheckbox.Visible = false;
+			on_init();
 		}
+		
+		private void on_init()
+		{
+			//add focus listener for image widget
+			//add event masks
+			ImgCheck.AddEvents( (int)Gdk.EventMask.AllEventsMask   );
+			ImgCheck.AddEvents( (int)Gdk.EventMask.FocusChangeMask );
+			ImgCheck.AddEvents( (int)Gdk.EventMask.KeyPressMask    );
+			ImgCheck.WidgetFlags       |= WidgetFlags.CanFocus;
+			
+			//add events
+			ECheckbox.ButtonPressEvent += new ButtonPressEventHandler( OnButtonPress );
+			ImgCheck.KeyPressEvent     += new KeyPressEventHandler   ( OnImgKeyPress );
+		}
+		
+		private void OnImgKeyPress( object sender, Gtk.KeyPressEventArgs e )
+		{
+			if( e.Event.Key.Equals( Gdk.Key.Return ) || e.Event.Key.Equals( Gdk.Key.space ) )
+				Checked = !Checked;
+		}
+
+		private void OnButtonPress( object sender, EventArgs args )
+		{
+			Checked = !Checked;
+		}
+		
+		private void OnCheckedStateChanged()
+		{
+			if( Checked )
+				ImgCheck.Pixbuf = CheckedImage;
+			else
+				ImgCheck.Pixbuf = UncheckedImage;
+			//
+			if( CheckedStateChanged != null )
+					CheckedStateChanged( this, new EventArgs() );
+		}
+
+		
+
+#region properties
+		public bool Checked 
+		{
+			get 
+			{
+				return _checked;
+			}
+			set 
+			{
+				_checked = value;
+				OnCheckedStateChanged();
+			}
+		}
+
+		public Pixbuf CheckedImage 
+		{
+			get 
+			{
+				return checkedImage;
+			}
+			set 
+			{
+				checkedImage    = value;
+				OnCheckedStateChanged();
+			}
+		}
+
+		public Pixbuf UncheckedImage 
+		{
+			get 
+			{
+				return uncheckedImage;
+			}
+			set 
+			{
+				uncheckedImage = value;
+				OnCheckedStateChanged();
+			}
+		}
+		
+		public String Text
+		{
+			get
+			{
+				return LblText.Text;
+			}
+			set
+			{
+				LblText.Text = value;
+			}
+		}
+#endregion
+		
 	}
 }
