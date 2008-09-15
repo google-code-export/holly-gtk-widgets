2f65b6695bd555beccc7b81a0aa4ee9e0e30d6bd
fr.daniil <fr.daniil@9c20132d-584d-0410-8be9-414f904cf137>
2008-09-15 11:16:17 +0000

    the HFileSelectorWidget

---
+++ trunk/HollyLibrary/FileSelector/HFileSelectorWidget.cs
@@ -0,0 +1,21 @@
+// HFileSelectorWidget.cs created with MonoDevelop
+// User: dantes at 9:51 PM 9/13/2008
+//
+// To change standard headers go to Edit->Preferences->Coding->Standard Headers
+//
+
+using System;
+
+namespace HollyLibrary
+{
+	
+	
+	public partial class HFileSelectorWidget : Gtk.Bin
+	{
+		
+		public HFileSelectorWidget()
+		{
+			this.Build();
+		}
+	}
+}
