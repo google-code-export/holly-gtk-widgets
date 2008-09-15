0c1e14982d1450dc3429e47bb985420cd4019e8b
fr.daniil <fr.daniil@9c20132d-584d-0410-8be9-414f904cf137>
2008-09-15 11:16:17 +0000

    the HFileSelectorWidget

---
+++ HollyLibrary/FileSelector/FileViewWidget.cs
@@ -0,0 +1,64 @@
+// FileViewWidget.cs created with MonoDevelop
+// User: dantes at 1:58 PM 9/15/2008
+//
+// To change standard headers go to Edit->Preferences->Coding->Standard Headers
+//
+
+using System;
+using System.IO;
+using Gtk;
+
+namespace HollyLibrary
+{
+
+	public enum FileViewTypes
+	{
+		Thumbnail, Tiles, List
+	}
+	
+	public class FileViewWidget : Gtk.Bin
+	{
+		ScrolledWindow view    = new ScrolledWindow();
+		FileViewTypes viewType = FileViewTypes.List;
+		String currentPath     = "";
+		
+		public FileViewWidget()
+		{
+			this.Add( view );
+		}
+		
+		public void Renderer()
+		{
+			//daca este List
+			if( ViewType == FileViewTypes.List )
+			{
+				Label lbl = new Label("");
+				
+			}
+		}
+		
+		public FileViewTypes ViewType 
+		{
+			get 
+			{
+				return viewType;
+			}
+			set 
+			{
+				viewType = value;
+			}
+		}
+
+		public string CurrentPath 
+		{
+			get 
+			{
+				return currentPath;
+			}
+			set 
+			{
+				currentPath = value;
+			}
+		}
+	}
+}
