889c27e7311a2a5446b0283330f8b4b71f2e2268
fr.daniil <fr.daniil@9c20132d-584d-0410-8be9-414f904cf137>
2008-09-18 09:50:17 +0000

    bug fixing on HLabel

--- trunk/test/MainWindow2.cs
+++ trunk/test/MainWindow2.cs
@@ -20,9 +20,17 @@ namespace test
 		{
 			this.Build();
 
-			himagecheckbox1.CheckedImage = GraphUtil.pixbufFromStock( "gtk-yes", Gtk.IconSize.Button );
-			himagecheckbox1.UncheckedImage = GraphUtil.pixbufFromStock( "gtk-no", Gtk.IconSize.Button );
+
+			hlabel1.TextOverwritesIcon = false;
+			hlabel1.HorizontalLine     = true;
+			hlabel1.TextPosition       = HPosition.Left;
+			hlabel1.IconPosition       = HPosition.Left;
 			
+			//for example, get the theme folder icon
+			Gtk.IconTheme theme = Gtk.IconTheme.Default;
+			Gdk.Pixbuf buf      = theme.LoadIcon("folder",24, Gtk.IconLookupFlags.ForceSvg);
+			//set icon for label
+			hlabel1.Icon        = buf;
 		}
 
 		protected virtual void OnDeleteEvent (object o, Gtk.DeleteEventArgs args)
--- trunk/test/gtk-gui/test.MainWindow2.cs
+++ trunk/test/gtk-gui/test.MainWindow2.cs
@@ -15,9 +15,17 @@ namespace test {
         
         private Gtk.VBox vbox1;
         
+        private Gtk.Button button4;
+        
         private Gtk.HBox hbox1;
         
-        private HollyLibrary.HImageCheckBox himagecheckbox1;
+        private Gtk.Button button3;
+        
+        private HollyLibrary.HLabel hlabel1;
+        
+        private Gtk.Button button2;
+        
+        private Gtk.Button button5;
         
         protected virtual void Build() {
             Stetic.Gui.Initialize(this);
@@ -30,20 +38,65 @@ namespace test {
             this.vbox1.Name = "vbox1";
             this.vbox1.Spacing = 6;
             // Container child vbox1.Gtk.Box+BoxChild
+            this.button4 = new Gtk.Button();
+            this.button4.CanFocus = true;
+            this.button4.Name = "button4";
+            this.button4.UseUnderline = true;
+            this.button4.Label = Mono.Unix.Catalog.GetString("button4");
+            this.vbox1.Add(this.button4);
+            Gtk.Box.BoxChild w1 = ((Gtk.Box.BoxChild)(this.vbox1[this.button4]));
+            w1.Position = 0;
+            w1.Expand = false;
+            w1.Fill = false;
+            // Container child vbox1.Gtk.Box+BoxChild
             this.hbox1 = new Gtk.HBox();
             this.hbox1.Name = "hbox1";
             this.hbox1.Spacing = 6;
             // Container child hbox1.Gtk.Box+BoxChild
-            this.himagecheckbox1 = new HollyLibrary.HImageCheckBox();
-            this.himagecheckbox1.Events = ((Gdk.EventMask)(256));
-            this.himagecheckbox1.Name = "himagecheckbox1";
-            this.himagecheckbox1.Checked = false;
-            this.hbox1.Add(this.himagecheckbox1);
-            Gtk.Box.BoxChild w1 = ((Gtk.Box.BoxChild)(this.hbox1[this.himagecheckbox1]));
-            w1.Position = 0;
-            this.vbox1.Add(this.hbox1);
-            Gtk.Box.BoxChild w2 = ((Gtk.Box.BoxChild)(this.vbox1[this.hbox1]));
+            this.button3 = new Gtk.Button();
+            this.button3.CanFocus = true;
+            this.button3.Name = "button3";
+            this.button3.UseUnderline = true;
+            this.button3.Label = Mono.Unix.Catalog.GetString("button3");
+            this.hbox1.Add(this.button3);
+            Gtk.Box.BoxChild w2 = ((Gtk.Box.BoxChild)(this.hbox1[this.button3]));
             w2.Position = 0;
+            w2.Expand = false;
+            w2.Fill = false;
+            // Container child hbox1.Gtk.Box+BoxChild
+            this.hlabel1 = new HollyLibrary.HLabel();
+            this.hlabel1.Name = "hlabel1";
+            this.hlabel1.LabelProp = Mono.Unix.Catalog.GetString("hlabel1");
+            this.hlabel1.HorizontalLine = false;
+            this.hlabel1.TextOverwritesIcon = false;
+            this.hbox1.Add(this.hlabel1);
+            Gtk.Box.BoxChild w3 = ((Gtk.Box.BoxChild)(this.hbox1[this.hlabel1]));
+            w3.Position = 1;
+            // Container child hbox1.Gtk.Box+BoxChild
+            this.button2 = new Gtk.Button();
+            this.button2.CanFocus = true;
+            this.button2.Name = "button2";
+            this.button2.UseUnderline = true;
+            this.button2.Label = Mono.Unix.Catalog.GetString("button2");
+            this.hbox1.Add(this.button2);
+            Gtk.Box.BoxChild w4 = ((Gtk.Box.BoxChild)(this.hbox1[this.button2]));
+            w4.Position = 2;
+            w4.Expand = false;
+            w4.Fill = false;
+            this.vbox1.Add(this.hbox1);
+            Gtk.Box.BoxChild w5 = ((Gtk.Box.BoxChild)(this.vbox1[this.hbox1]));
+            w5.Position = 1;
+            // Container child vbox1.Gtk.Box+BoxChild
+            this.button5 = new Gtk.Button();
+            this.button5.CanFocus = true;
+            this.button5.Name = "button5";
+            this.button5.UseUnderline = true;
+            this.button5.Label = Mono.Unix.Catalog.GetString("button5");
+            this.vbox1.Add(this.button5);
+            Gtk.Box.BoxChild w6 = ((Gtk.Box.BoxChild)(this.vbox1[this.button5]));
+            w6.Position = 2;
+            w6.Expand = false;
+            w6.Fill = false;
             this.Add(this.vbox1);
             if ((this.Child != null)) {
                 this.Child.ShowAll();
